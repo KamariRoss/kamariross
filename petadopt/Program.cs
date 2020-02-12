8ddf3406b8f2ad165d6361e8415b075c63622355
KamariRoss <kamarimobile@gmail.com>
2020-02-12 11:28:54 -0500

    My pets can be added

---
+++ petadopt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace petadopt
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var shelter = new List<string>();
+      while (true)
+      {
+        Console.WriteLine("Current Pets");
+        Console.WriteLine($"There are{ shelter.Count} pets.");
+        for (var i = 0; i < shelter.Count; i++)
+        {
+          var currentPet = shelter[i];
+          Console.WriteLine($"(i): {currentPet}");
+        }
+        Console.WriteLine("what pet do you want to add?");
+        var pet = Console.ReadLine();
+
+        shelter.Add(pet);
+      }
+
+    }
+  }
+}
